2102bd670bc36d57236bcd984b8d2315f66a1b2d
mookeun.ji <goofcode@gmail.com>
2017-05-17 15:01:22 +0900

    Add feature to draw rectangle while dragging

--- Rector/src/MainFrame.java
+++ Rector/src/MainFrame.java
@@ -6,14 +6,15 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import org.w3c.dom.css.Rect;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +22,7 @@ import java.io.IOException;
 public class MainFrame extends JFrame {
 	
 	private JPanel imagePanel;
+	
 	private JPanel buttonPanel;
 	private JButton leftButton;
 	private JButton rightButton;
@@ -31,6 +33,7 @@ public class MainFrame extends JFrame {
 	private ImageGetter imageGetter;
 	
 	private File currentImg;
+	private int imgWidth, imgHeight;
 	
 	public MainFrame(ImageGetter imageGetter, RectMaker rectMaker) {
 		
@@ -40,54 +43,22 @@ public class MainFrame extends JFrame {
 		setTitle("Rect Maker");
 		setLayout(new BorderLayout());
 		
+		/* img panel */
 		imagePanel = new JPanel(new BorderLayout());
 		attachImage(imageGetter.getCurrentImage());
-		imagePanel.addMouseListener(new MouseListener() {
-			private int startX, startY;
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				rectMaker.appendRect(currentImg, startX, startY, e.getX(), e.getY() );
-			}
-			@Override
-			public void mousePressed(MouseEvent e) {
-				startX = e.getX(); startY = e.getY();
-			}
-			public void mouseClicked(MouseEvent e) {}
-			public void mouseEntered(MouseEvent e) {}
-			@Override
-			public void mouseExited(MouseEvent e) {
-			}
-		});
+		ImagePanelMouseListener imagePanelMouseListener = new ImagePanelMouseListener();
+		imagePanel.addMouseListener(imagePanelMouseListener);
+		imagePanel.addMouseMotionListener(imagePanelMouseListener);
 		add(imagePanel, BorderLayout.CENTER);
 
+		/* button panel */
 		buttonPanel = new JPanel();
 		leftButton = new JButton("<");
 		rightButton = new JButton(">");
 		saveButton = new JButton("Save");
 		undoButton = new JButton("Ctrl + z");
-		leftButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try{
-					attachImage(imageGetter.getPreviousImage());
-				}
-				catch(Exception ex){
-					JOptionPane.showMessageDialog(null, ex.getMessage());
-				}
-			}
-		});
-		rightButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try{
-					attachImage(imageGetter.getNextImage());
-				}
-				catch(Exception ex){
-					JOptionPane.showMessageDialog(null, ex.getMessage());
-				}
-			}
-		});
+		leftButton.addActionListener(new arrowButtonButtonListener(true));
+		rightButton.addActionListener(new arrowButtonButtonListener(false));
 		saveButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -111,7 +82,6 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		pack();
-		//setSize(640, 480);
 		setLocation(400,400);
 		setVisible(true);
 	}
@@ -126,12 +96,83 @@ public class MainFrame extends JFrame {
 			imagePanel.removeAll();
 			
 			imagePanel.add(new JLabel(imageIcon), BorderLayout.CENTER);
-			imagePanel.setSize(bufferedImage.getWidth(), bufferedImage.getHeight());
+			
+			imgWidth = bufferedImage.getWidth();
+			imgHeight = bufferedImage.getHeight();
+			imagePanel.setSize(imgWidth, imgHeight);
+			
+			
 			imagePanel.revalidate();
 			imagePanel.repaint();
+			System.out.println(img.getName()+"("+bufferedImage.getWidth() + "*" + bufferedImage.getHeight()+")");
+			
 		}
 		 catch (IOException ioEx) {
 			ioEx.printStackTrace();
 		 }
+	}
+	
+	class ImagePanelMouseListener implements MouseListener,MouseMotionListener{
+
+		private int startX, startY;
+		private int endX, endY;
+		private Graphics graphics;
+		
+		
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			
+			if(e.getX() < 0) endX = 0;
+			else if(e.getX() > imgWidth) endX = imgWidth;
+			else endX = e.getX();
+			
+			if(e.getY() < 0) endY = 0;
+			else if(e.getY() > imgHeight) endY = imgHeight;
+			else endY = e.getY();
+
+			rectMaker.appendRect(currentImg, startX, startY, endX, endY);
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			startX = e.getX(); startY = e.getY();
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			graphics = imagePanel.getGraphics();
+
+			graphics.drawLine(startX, startY, startX, e.getY());
+			graphics.drawLine(startX, startY, e.getX(), startY);
+			graphics.drawLine(startX, e.getY(), e.getX(), e.getY());
+			graphics.drawLine(e.getX(), startY, e.getX(), e.getY());
+			imagePanel.repaint();
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		@Override
+		public void mouseMoved(MouseEvent e) {
+		}
+	}
+	class arrowButtonButtonListener implements ActionListener{
+
+		private boolean isLeft;
+		
+		public arrowButtonButtonListener(boolean isLeft) {
+			this.isLeft = isLeft; 
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try{
+				if(isLeft) attachImage(imageGetter.getPreviousImage());
+				else attachImage(imageGetter.getNextImage());
+			}
+			catch(Exception ex){
+				JOptionPane.showMessageDialog(null, ex.getMessage());
+			}
+		}
+		
 	}
 }
